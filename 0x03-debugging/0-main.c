430e6fad8a64d0ca53d0511160403cb93511d7a8
Louie-pixel <louiewilli478@gmail.com>
2023-10-13 13:33:51 -0700

    12

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * main - Test function for positive or negative
- * Return: 0
- */
-
+* main - Test function for positive or negative
+* Return: 0
+*/
 int main(void)
 {
         int i;
@@ -12,6 +10,5 @@ int main(void)
         i = 0;
 
         positive_or_negative(i);
-
 return (0);
 }
