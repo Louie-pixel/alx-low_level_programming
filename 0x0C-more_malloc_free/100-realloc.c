e29082e0b194bbdd2933f10072c7922d4f15d288
Louie_pixel <louiewilli478@gmail.com>
2023-11-02 11:40:51 -0700

    123

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,54 +1,38 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
+    void *new_ptr;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
+    if (new_size == 0) {
+        free(ptr);
+        return NULL;
     }
-    printf("\n");
+
+    if (ptr == NULL) {
+        return malloc(new_size);
+    }
+
+    if (new_size == old_size) {
+        return ptr;
+    }
+
+    new_ptr = malloc(new_size);
+
+    if (new_ptr == NULL) {
+        return NULL;
+    }
+
+    // Copy the contents from the old memory block to the new one
+    if (new_size > old_size) {
+        new_size = old_size;
+    }
+
+    for (unsigned int i = 0; i < new_size; i++) {
+        ((char *)new_ptr)[i] = ((char *)ptr)[i];
+    }
+
+    free(ptr);  // Free the old memory block
+
+    return new_ptr;
 }
 
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *p;
-    int i;
-
-    p = malloc(sizeof(char) * 10);
-    p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
-    i = 0;
-    while (i < 98)
-    {
-        p[i++] = 98;
-    }
-    simple_print_buffer(p, 98);
-    free(p);
-    return (0);
-}
