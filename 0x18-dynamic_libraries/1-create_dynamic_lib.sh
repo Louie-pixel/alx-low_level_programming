c08fd812b66f59936a40d70c550d4aa72bc2709c
Louie_pixel <louiewilli478@gmail.com>
2024-01-15 23:01:29 -0800

    d

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+
+# Compile each .c file (excluding _putchar.c) into individual object files
+for file in *.c; do
+  if [ "$file" != "_putchar.c" ]; then
+    gcc -Wall -fPIC -c "$file"
+  fi
+done
+
+# Create the dynamic library liball.so
 gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
